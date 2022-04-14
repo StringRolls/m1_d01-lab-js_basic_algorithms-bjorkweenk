bf207e2a78cb3646ed8ca4e6888d4944cfbe468d
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-04-14 11:45:42 +0200

    bonus1

--- js/index.js
+++ js/index.js
@@ -2,11 +2,9 @@
 
 
 
-// Bonus 1: Palindromes
 // Bonus 2: Lorem ipsum counter
 
 // Iteration 1: Names and Input
-
 let hacker1 = "Bjork" ;
 console.log(`The drivers name is ${hacker1}`);
 
@@ -35,7 +33,7 @@ for (let i = 0; i < hacker1.length; i++){
 }
 console.log(spacedOut.toUpperCase());
 
-// Iteration 3.2 'Name' to 'emaN'
+// Iteration 3.2 'Name' to 'eman'
 
 let reversed = "";
 for (let i = hacker1.length -1; i >=0; i--){
@@ -52,3 +50,21 @@ else if (hacker1<hacker2){
 else if (hacker1 == hacker2){
     console.log('What?! You both have the same?')
 }
+
+// Bonus 1: Palindromes
+
+let paragraph1 = <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra hendrerit ante, ut congue lorem imperdiet ut. Morbi tempor consequat libero. Pellentesque sit amet justo ut lectus condimentum blandit in vel tortor. Aliquam lobortis tristique orci at cursus. Sed dictum odio eu arcu hendrerit gravida. Aliquam bibendum sodales molestie. Morbi pulvinar faucibus elit in ullamcorper. Phasellus et blandit felis. Vestibulum mattis tincidunt metus non varius.</p>
+
+let paragraph2 = <p> Pellentesque tempus quam a ipsum sodales commodo. Integer egestas eleifend erat ut euismod. Etiam ante ex, placerat sit amet dui sed, dapibus vulputate urna. In rhoncus magna id eros congue, at venenatis odio consectetur. In congue luctus odio ac tristique. Nunc nibh nunc, ornare quis facilisis elementum, aliquet sed libero. Duis scelerisque velit nec leo congue, ac consectetur erat interdum. Mauris ex nunc, tristique eget neque vel, rutrum pretium elit. Vestibulum viverra dignissim velit, in sodales dolor suscipit in. Aliquam sem lectus, tincidunt non pulvinar et, posuere et felis. Maecenas id elementum odio. </p>
+
+let paragraph3 = <p> Sed ut sodales elit. Phasellus sed purus dignissim, fermentum dui sed, pulvinar sapien. Etiam et luctus lectus. Nullam id ex magna. Quisque ac condimentum urna. Nulla consequat faucibus ante ut finibus. Vestibulum tellus sem, aliquam sed vulputate in, accumsan ut turpis. Quisque gravida maximus suscipit. In a nisl ac sem dignissim semper a et nisi. </p>
+
+function WordCount (str){
+    var totalSoFar = 0;
+    for ( var i=0; i < WordCount.length, i++)
+    if(str(i)=== " "){
+        totalSoFar = +1;
+    }
+}
+
+console.log(WordCount("Random String"));
